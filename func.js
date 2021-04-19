bef29ef6455c5fd18e328d84e3367b5b50ac183a
A03HCY <59499875+A03HCY@users.noreply.github.com>
2021-04-19 21:52:15 +0800

    Add files via upload

--- func.js
+++ func.js
@@ -20,3 +20,52 @@ function StartEnvir() {
         Auto();
     }
 }
+
+function MyBirth(){
+    var birth='2006-11-30';
+    birth = Date.parse(birth.replace('/-/g', "/"));
+    if (birth) {
+        var year = 1000 * 60 * 60 * 24 * 365;
+        var now = new Date();
+        var birthday = new Date(birth);
+        var age = parseInt((now - birthday) / year);
+        return age
+    }
+}
+
+function HT(FN, tit) {
+    var oli = document.getElementsByTagName("li");
+    for(var i=0; i<oli.length; i++){
+        if (oli[i].id == "d-bran"){
+            continue;
+        }
+        oli[i].setAttribute("class","mdui-list-item mdui-ripple");
+    };
+    document.getElementById("f-title").innerHTML=tit
+    document.getElementById("title").innerHTML="Acdp | "+tit
+    mdui.$('article').html(
+        '<div class="mdui-text-center"><div class="mdui-spinner mdui-spinner-colorful"></div></div>'
+    );
+    mdui.mutation();
+    var xmlhttp;
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        xmlhttp = new ActiveXObject('Microsoft.XMLHttp');
+    }
+    xmlhttp.onreadystatechange = function() {
+        if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            mdui.$('article').html('');
+            if(MD == true) {
+                document.getElementById("divx").innerHTML(xmlhttp.responseText);
+            } else {
+                mdui.snackbar({
+                    message: "Can't Request!",
+                    position: 'right-bottom'
+                });
+            }
+        }
+    }
+    xmlhttp.open('GET', FN+"?timestamp="+new Date().getTime(), true);
+    xmlhttp.send();
+}
