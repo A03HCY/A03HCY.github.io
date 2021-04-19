7f4eda6cd55ec634d26188b2db47e54d37205870
A03HCY <59499875+A03HCY@users.noreply.github.com>
2021-04-19 22:38:23 +0800

    Add files via upload

--- func.js
+++ func.js
@@ -1,23 +1,27 @@
 function Dark() {
     SetCookie("envir","dark")
+    document.getElementById("sn").innerHTML = '<i class="mdui-icon material-icons">brightness_2</i>';
     document.getElementById("body").setAttribute("class","mdui-appbar-with-toolbar mdui-drawer-body-left mdui-theme-layout-dark")
 }
 function Light() {
     SetCookie("envir","light")
+    document.getElementById("sn").innerHTML = '<i class="mdui-icon material-icons">brightness_high</i>';
     document.getElementById("body").setAttribute("class","mdui-appbar-with-toolbar mdui-drawer-body-left")
-}
-function Auto() {
-    SetCookie("envir","auto")
-    document.getElementById("body").setAttribute("class","mdui-appbar-with-toolbar mdui-drawer-body-left mdui-theme-layout-auto")
 }
 function StartEnvir() {
     var en = GetCookie("envir");
     if(en == "light") {
         Light();
-    } else if(en == "dark") {
+    } else{
         Dark();
-    } else {
-        Auto();
+    }
+}
+function ChangeL() {
+    var en = GetCookie("envir");
+    if(en == "light") {
+        Dark();
+    } else{
+        Light();
     }
 }
 
