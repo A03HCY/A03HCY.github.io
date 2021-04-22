76ed0188bc4ec5679bf0d088d11107de5e600e15
A03HCY <59499875+A03HCY@users.noreply.github.com>
2021-04-22 23:05:57 +0800

    Update func.js

--- func.js
+++ func.js
@@ -24,12 +24,12 @@ function StartEnvir() {
 function ChangeV() {
     var v = document.getElementById("slider").value;
     document.getElementById("hidden").volume = v;
-    document.getElementById("story-m").volume = v + 0.08;
+    document.getElementById("story-m").volume = parseFloat(v) + 0.08;
 }
 function InitM() {
     var muv = 0.15;
     document.getElementById("hidden").volume = muv;
-    document.getElementById("story-m").volume = muv + 0.08;
+    document.getElementById("story-m").volume = parseFloat(muv) + 0.08;
     document.getElementById("slider").value = muv;
     mdui.updateSliders();
 }
@@ -78,6 +78,7 @@ var story = 0;
 function Story(FN, id, url) {
     Active(FN, id);
     document.getElementById("story-m").setAttribute("src",url);
+    document.getElementById("story-m").play();
     if(story == 0) {
         if(GetCookie("music-od") != "true") {
             mdui.snackbar({
